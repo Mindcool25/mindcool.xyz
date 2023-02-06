f3940792d864c6b677250504dede03f05115ef17
Mindcool25 <mindcool@mindcool.xyz>
2023-02-05 20:59:38 -0700

    Made logo clickable

--- src/components/Navbox.jsx
+++ src/components/Navbox.jsx
@@ -4,12 +4,14 @@ import { Outlet, Link } from "react-router-dom";
 function NavBar() {
     return(
         <div className="sitemap">
+            <Link to={'/'}>
             <img className="icon" src="/logo_faviconv5.svg"/>
+            </Link>
             <h2>Mindcool's Site</h2>
-            <Link to={'/contact'}>Contact</Link>
-            <Link to={'/about'}>About</Link>
-            <Link to={'/projects'}>Projects</Link>
-            <Link to={'/'}>Home</Link>
+            <Link to={'/contact'} className="page">Contact</Link>
+            <Link to={'/about'} className="page">About</Link>
+            <Link to={'/projects'} className="page">Projects</Link>
+            <Link to={'/'} className="page">Home</Link>
         </div>
     )
 }
