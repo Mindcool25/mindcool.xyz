23663a5ae3c18cbf16857f9a7d9ce3396dd31098
Mindcool25 <mindcool@mindcool.xyz>
2023-02-05 20:48:53 -0700

    Added temp project description

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -5,6 +5,7 @@ function Projects() {
         <div>
             <NavBar/>
             <h1>Projects</h1>
+            <p>There will be a list of projects here soonish.</p>
         </div>
     );
 }
